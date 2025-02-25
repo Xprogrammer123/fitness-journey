22b36c5121ff91675d8ced11b141bbfb9f49344d
Code-lab-dev <fawassaka862@gmail.com>
2025-02-25 14:03:32 -0500

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -60,7 +60,7 @@ export const Sidebar = () => {
   return (
     <>
       {/* Desktop Sidebar */}
-      <div className="hidden md:flex h-screen w-64 xl:w-80 border-r border-gray-800">
+      <div className="hidden md:flex h-screen w-56 xl:w-80 border-r border-gray-800">
         <SidebarContent />
       </div>
 
@@ -71,7 +71,7 @@ export const Sidebar = () => {
             <Menu className="w-5 h-5" />
           </Button>
         </SheetTrigger>
-        <SheetContent side="left" className="w-64 p-0">
+        <SheetContent side="left" className="w-56 p-0">
           <SidebarContent />
         </SheetContent>
       </Sheet>
